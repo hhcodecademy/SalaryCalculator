053760ad4d45d2063cedf47e117beee8aacb1232
Huseyn Hasanli <h.hasanli@pa.gov.az>
2023-04-06 12:23:15 +0400

    add taxRate

--- SalaryCalculator/Program.cs
+++ SalaryCalculator/Program.cs
@@ -9,13 +9,14 @@ namespace SalaryCalculator
             float grossSalary = 0f;
             Console.WriteLine("Hello World!");
             Console.WriteLine("Please enter total gross salary");
-            float salary = float.Parse(Console.ReadLine());
+            grossSalary = float.Parse(Console.ReadLine());
             Console.WriteLine("Please enter married status (e / E: evli, s / S: subay, d / D: dul)");
             char marriedStatus  = char.Parse(Console.ReadLine().ToLower());
             int childCount = 0;
             if (marriedStatus=='e'||marriedStatus=='d')
             {
-                childCount= int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter child count");
+                childCount = int.Parse(Console.ReadLine());
             }
             Console.WriteLine("Please enter disabled status (b / B: bəli, x / X: yox))");
             char disabledStatus = char.Parse(Console.ReadLine().ToLower());
@@ -75,7 +76,7 @@ namespace SalaryCalculator
             }
             if (disabledStatus=='b')
             {
-                netSalary = netSalary + (taxAmount / 2);
+                 netSalary= netSalary + (taxAmount / 2);
                 taxAmount = taxAmount / 2;
             }
 
