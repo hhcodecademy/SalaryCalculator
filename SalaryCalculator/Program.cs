9f997d065a1f8bf5ec2461aa91eaea9e234d3b5b
Huseyn Hasanli <h.hasanli@pa.gov.az>
2023-04-06 11:36:23 +0400

    calculate net salary and tax rate

--- SalaryCalculator/Program.cs
+++ SalaryCalculator/Program.cs
@@ -10,14 +10,14 @@ namespace SalaryCalculator
             Console.WriteLine("Hello World!");
             Console.WriteLine("Please enter total gross salary");
             float salary = float.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter married status (e / E: evli, b / B: subay, d / D: dul)");
+            Console.WriteLine("Please enter married status (e / E: evli, s / S: subay, d / D: dul)");
             char marriedStatus  = char.Parse(Console.ReadLine().ToLower());
             int childCount = 0;
             if (marriedStatus=='e'||marriedStatus=='d')
             {
                 childCount= int.Parse(Console.ReadLine());
             }
-            Console.WriteLine("Please enter disabled status (e / E: bəli, x / X: yox))");
+            Console.WriteLine("Please enter disabled status (b / B: bəli, x / X: yox))");
             char disabledStatus = char.Parse(Console.ReadLine().ToLower());
 
             float marriedIncome = 0;
@@ -51,6 +51,34 @@ namespace SalaryCalculator
                     break;
             }
             grossSalary = grossSalary + childIncome;
+
+            float taxAmount = 0f;
+            float netSalary = 0;
+            float taxRate = 0;
+            if (grossSalary<=1000)
+            {
+                taxRate = 0.15f;
+                taxAmount = grossSalary * taxRate;                
+                netSalary = grossSalary * (1-taxRate);
+            }
+            else if (grossSalary > 1000 && grossSalary <= 2000)
+            {
+                taxRate = 0.2f;
+                taxAmount = grossSalary * taxRate;
+                netSalary = grossSalary * (1 - taxRate);
+            }
+            else if (grossSalary > 2000 && grossSalary <= 3000)
+            {
+                taxRate = 0.3f;
+                taxAmount = grossSalary * taxRate;
+                netSalary = grossSalary * (1 - taxRate);
+            }
+            if (disabledStatus=='b')
+            {
+                netSalary = netSalary + (taxAmount / 2);
+                taxAmount = taxAmount / 2;
+            }
+
         }
     }
 }
